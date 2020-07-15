c78454177c8dafa2e80d750e13fcdd9731f2e04d
cuongembaubang <cuongembaubang@gmail.com>
2020-07-15 13:40:26 +0700

    haiz

---
+++ WebsiteTinTuc/Controllers/HienThiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteTinTuc.Controllers
+{
+    public class HienThiController : Controller
+    {
+        // GET: HienThi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebsiteTinTuc/Controllers/QuangCaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteTinTuc.Controllers
+{
+    public class QuangCaoController : Controller
+    {
+        // GET: QuangCao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
